7e6044070f3051719fa3d8c15b32a61ce4742a20
Hakk Zuberbarg <hakansoderberg@MacBook-Air-som-tillhor-Hakk.local>
2025-03-13 21:38:30 +0100

    fix lint

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./component/header";
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
