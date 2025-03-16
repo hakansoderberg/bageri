2b12b52a118a0ccc218d7f3f8eb509afaa4e4ca5
Hakk Zuberbarg <hakansoderberg@MacBook-Air-som-tillhor-Hakk.local>
2025-03-16 17:28:34 +0100

    chacnge colr

--- app/component/highlight-box.tsx
+++ app/component/highlight-box.tsx
@@ -5,7 +5,7 @@ interface Props {
 const HighlightBox = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className=" inline-block border-orange-500 bg-orange-100 p-4 rounded-sm shadow-md">
-            <div className="border-2 border-amber-500 p-4 rounded-sm">
+            <div className="border-2 border-gray-900 p-4 rounded-sm">
                 {children}
             </div>
         </div>
