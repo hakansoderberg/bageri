c36612236912283c5b6cc7dacb84b3c68066d6ec
Hakk Zuberbarg <hakansoderberg@MacBook-Air-som-tillhor-Hakk.local>
2026-07-31 20:03:33 +0200

    lunch page

--- app/component/header.tsx
+++ app/component/header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
             <div className="flex flex-col md:flex-row md:justify-between md:items-center items-start px-4 md:px-8 py-4 md:py-8">
                 <div className="flex items-center mb-4 md:mb-0">
                     <Image className="pl-4" src="/images/ch-logo.png" alt="Haugen baugen logo" width={50} height={50} />
-                    <h1 className="inline text-base mb-0 pl-2">Gärdehov Restaurang & Catering</h1>
+                    <h1 className="inline text-sm md:text-base mb-0 pl-2">Gärdehov Restaurang & Catering</h1>
                 </div>
                 <div className="md:hidden">
                     <NavBarMobile />
--- app/lunch/page.tsx
+++ app/lunch/page.tsx
@@ -1,15 +1,24 @@
-import Block from "../component/block";
+import ContentSection from "../component/content-section";
+import HighlightBox from "../component/highlight-box";
+import InfoCard from "../component/info-card";
 import content from "../content/lunch-page.json";
+import mainContent from "../content/main.json";
 
 const Lunch = () => {
     
     return (
-        <div>
-            <Block className="mb-8 md:mb-8 " dark>
-                <h1 className="text-light">{content.title}</h1>
-                <p className="text-light">{content.text1}</p>
-            </Block>
-         
+        <div className="flex flex-col items-center">
+            <ContentSection title={content.title} text={content.text} button={{label:mainContent.lunchButtonText, href:mainContent.lunchButtonLink}} />
+            <div className="flex flex-col items-center justify-between mb-8 md:mb-16">
+                    <HighlightBox className="w-fit text-center wrapper-line p-8">
+                        <p className="text-base text-light inline font-merry">{mainContent.openingHours.title1}</p>
+                        <p className="text-base text-pale inline ml-2 ">{mainContent.openingHours.title2}</p>
+                        <p className="text-xs pt-2 mb-0">{mainContent.openingHours.text}</p>
+                    </HighlightBox>
+                </div>
+            <div className="w-full md:w-1/2 text-center p-4 md:p-0">
+                <InfoCard title={content.infoTitle} text={content.infoRows} />
+            </div>
         </div>
     );
 };
