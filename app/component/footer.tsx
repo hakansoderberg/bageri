f7952236f4ba58f736ea2418c40caaeecba91250
Hakk Zuberbarg <hakansoderberg@MacBook-Air-som-tillhor-Hakk.local>
2026-07-31 20:17:02 +0200

    footer

--- app/component/footer.tsx
+++ app/component/footer.tsx
@@ -1,41 +1,30 @@
 import Link from "next/link";
-
+const footerLinkStyle = "hover:text-black-100 footer-link";
+const footerTextStyle = "footer-link";
 const Footer = () => {
     return (
         <footer className="bg-secondary pb-8">
             <div className="container mx-auto px-6 max-w-screen-md pt-8">
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                     <div>
-                        <h3 className="text-lg font-semibold mb-4">Kontakt</h3>
+                        <h3 className="text-base font-semibold mb-4">Kontakt</h3>
                         <ul >
-                            <li>Företagsnamn AB</li>
-                            <li>Adress: Gatan 123, 123 45 Stad</li>
-                            <li>Telefon: 012-345 67 89</li>
-                            <li>Email: kontakt@foretag.com</li>
+                            <li className={footerTextStyle}>Företagsnamn AB</li>
+                            <li className={footerTextStyle}>Adress: Gatan 123, 123 45 Stad</li>
+                            <li className={footerTextStyle}>Telefon: 012-345 67 89</li>
+                            <li className={footerTextStyle}>Email: kontakt@foretag.com</li>
                         </ul>
                     </div>
 
                     <div>
-                        <h3 className="text-lg font-semibold mb-4">Länkar</h3>
-                        <ul>
-                            <li>
-                                <Link href="/om-oss">Om oss</Link>
-                            </li>
-                            <li>
-                                <Link href="/kontakt">Kontakta</Link>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <div>
-                        <h3 className="text-lg font-semibold mb-4">Följ oss</h3>
+                        <h3 className="text-base font-semibold mb-4">Följ oss</h3>
                         <ul>
                             <li>
                                 <a
                                     href="https://www.facebook.com"
                                     target="_blank"
                                     rel="noopener noreferrer"
-                                    className="hover:text-blue-500"
+                                    className={footerLinkStyle}
                                 >
                                     Facebook
                                 </a>
@@ -45,7 +34,7 @@ const Footer = () => {
                                     href="https://www.instagram.com"
                                     target="_blank"
                                     rel="noopener noreferrer"
-                                    className="hover:text-pink-500"
+                                     className={footerLinkStyle}
                                 >
                                     Instagram
                                 </a>
