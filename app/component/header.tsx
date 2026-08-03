083d3ccda328faaaf2c9f184185703f42cc6a9e9
Hakk Zuberbarg <hakansoderberg@MacBook-Air-som-tillhor-Hakk.local>
2026-08-03 21:00:45 +0200

    link to mainpage and add favixon

--- app/component/header.tsx
+++ app/component/header.tsx
@@ -1,3 +1,5 @@
+"use client";
+import Link from "next/link";
 import Image from "next/image";
 import NavBarDesktop from "./nav-bar-desktop";
 import NavBarMobile from "./nav-bar-mobile";
@@ -6,10 +8,13 @@ const Header = () => {
     return (
         <header className="bg-dark py-4 md:mb-16 ">
             <div className="flex flex-col md:flex-row md:justify-between md:items-center items-start px-4 md:px-8 py-4 md:py-8">
+                <Link href="/">
                 <div className="flex items-center mb-4 md:mb-0">
                     <Image className="pl-4" src="/images/ch-logo.png" alt="Haugen baugen logo" width={50} height={50} />
                     <h1 className="inline text-sm md:text-base mb-0 pl-2">Gärdehov Restaurang & Catering</h1>
                 </div>
+                
+                </Link>
                 <div className="md:hidden">
                     <NavBarMobile />
                 </div>
